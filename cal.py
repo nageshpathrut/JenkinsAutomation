1c7741b6b7de346836eaf02b40eaf68a70caa263
nageshpathrut <nkpathrut.nbnscoe@gmail.com>
2025-10-04 09:43:34 +0530

    Create cal.py
    calculator app

---
+++ cal.py
@@ -0,0 +1,13 @@
+def calculator(a, b, operation):
+    if operation == "add":
+        return a + b
+    elif operation == "subtract":
+        return a - b
+    elif operation == "multiply":
+        return a * b
+    elif operation == "divide":
+        if b == 0:
+            raise ValueError("Division by zero is not allowed")
+        return a / b
+    else:
+        raise ValueError("Invalid operation")
